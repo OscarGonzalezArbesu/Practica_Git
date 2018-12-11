5d731d24aa2b04bb549437750218530ae503fd65
Oscar <oscargonzalezarbesu@gmail.com>
2018-12-11 12:03:06 +0100

    PUNTO 2

--- src/proyecto/Reloj.java
+++ src/proyecto/Reloj.java
@@ -1,5 +1,12 @@
 package proyecto;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Reloj {
 	private int Nombre;
 	private int Apellidos;
@@ -28,5 +35,27 @@ public class Reloj {
 	public void setDni(int dni) {
 		Dni = dni;
 	}
-	
+	public void write() {
+		// Copiar ficheros
+		File origen = new File("origen.txt");
+		File destino = new File("destino.txt");
+
+		try {
+		  InputStream in = new FileInputStream(origen);
+		  OutputStream out = new FileOutputStream(destino);
+						
+		  byte[] buf = new byte[1024];
+		  int len;
+
+		  while ((len = in.read(buf)) > 0) {
+		    out.write(buf, 0, len);
+		  }
+				
+		  in.close();
+		  out.close();
+		} catch (IOException ioe){
+		  ioe.printStackTrace();
+		}
+
+	}
 }
